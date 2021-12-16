22688610083a9d3ea04e20fd8298bb22428642da
sleepypanda <cheeyang85@gmail.com>
2021-12-16 13:11:32 +0800

    Game now supports full screen for different aspect ratios. This is implemented with a camera that has the original size of 1280x720 and changing window stretch aspect from keep to expand. Circle transition's size will now change depending on the screen's size.

--- Game/MainMenu/MainMenu.gd
+++ Game/MainMenu/MainMenu.gd
@@ -9,7 +9,7 @@ func _ready() -> void:
 	screen_transition.transition_in()
 	start.connect("clicked_start", self, "on_clicked_start")
 	screen_transition.connect("transitioned_out", self, "go_to_next_scene")
-	
+	screen_metrics()
 
 func on_clicked_start() -> void:
 	screen_transition.transition_out()
@@ -17,3 +17,13 @@ func on_clicked_start() -> void:
 
 func go_to_next_scene() -> void:
 	get_tree().change_scene(next_scene)
+
+
+func screen_metrics():
+	print("                 [Screen Metrics]")
+	print("            Display size: ", OS.get_screen_size())
+	print("   Decorated Window size: ", OS.get_real_window_size())
+	print("             Window size: ", OS.get_window_size())
+	print("        Project Settings: Width=", ProjectSettings.get_setting("display/window/size/width"), " Height=", ProjectSettings.get_setting("display/window/size/height")) 
+	print(OS.get_window_size().x)
+	print(OS.get_window_size().y)
--- Shaders/CircleScreenTransition/CircleScreenTransition.gd
+++ Shaders/CircleScreenTransition/CircleScreenTransition.gd
@@ -4,18 +4,26 @@ onready var tween = $Tween
 var black_screen : bool = true
 signal transitioned_out
 
+
+func _ready():
+	# Having it here helps update the rect_min_size upon changing to a new scene
+	# if window size would to be changed
+	rect_min_size = OS.get_real_window_size()
+	
+
 func transition_in() -> void:
-	material.set_shader_param("screen_width", 1280)
-	material.set_shader_param("screen_height", 720)
-	tween.interpolate_property(material, "shader_param/circle_size", 0, 1.05, 0.5, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
+	print(OS.get_real_window_size())
+	material.set_shader_param("screen_width", rect_min_size.x)
+	material.set_shader_param("screen_height", rect_min_size.y)
+	tween.interpolate_property(material, "shader_param/circle_size", 0, 1.05, 0.75, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
 	tween.start()
 	black_screen = false
 
 
 func transition_out() -> void:
-	material.set_shader_param("screen_width", 1280)
-	material.set_shader_param("screen_height", 720)
-	tween.interpolate_property(material, "shader_param/circle_size", 1.05, 0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
+	material.set_shader_param("screen_width", rect_min_size.x)
+	material.set_shader_param("screen_height", rect_min_size.y)
+	tween.interpolate_property(material, "shader_param/circle_size", 1.05, 0, 0.75, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
 	tween.start() 
 	black_screen = true
 
