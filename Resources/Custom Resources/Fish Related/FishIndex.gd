8019b42643ed5ae48870c569f96d686708af1d5f
sleepypanda <cheeyang85@gmail.com>
2022-02-21 22:36:53 +0800

    Added FishIndex and FishInfo resources for index. Added FishIndex mockup (wip)

---
+++ Resources/Custom Resources/Fish Related/FishIndex.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name FishIndex
+
+export(Array, Resource) var items
+
---
+++ Resources/Custom Resources/Fish Related/Fishes/FishInfo.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name FishInfo
+
+export(String) var name
+export(Texture) var sprite
+export(bool) var can_touch
+var times_caught : int
+export(String, MULTILINE) var description
+
