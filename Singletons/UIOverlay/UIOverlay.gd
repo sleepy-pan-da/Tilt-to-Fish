1a3200082090c62c2ccd3af7c4d4df6aff7daf75
sleepypanda <cheeyang85@gmail.com>
2022-08-02 21:12:42 +0800

    Update UIOverlay.gd
    -commented out some lines

--- Singletons/UIOverlay/UIOverlay.gd
+++ Singletons/UIOverlay/UIOverlay.gd
@@ -8,20 +8,20 @@ var popup_queue = []
 func _ready() -> void:
 	popup_panel.connect("popup_hide", self, "update_pop_up_text_and_show_from_front_of_popup_queue")
 
-func _process(delta) -> void:
-	if Input.is_action_just_pressed("ui_select"):
-		add_to_popup_queue(default_prophecy)
+#func _process(delta) -> void:
+#	if Input.is_action_just_pressed("ui_select"):
+#		add_to_popup_queue(default_prophecy)
 
 func add_to_popup_queue(prophecy : Prophecy) -> void:
 	popup_queue.push_back(prophecy)
 	if !popup_panel.visible:
 		update_pop_up_text_and_show_from_front_of_popup_queue()
-	print("Current size of popup_queue: " + str(popup_queue.size()))
+	#print("Current size of popup_queue: " + str(popup_queue.size()))
 	
 func update_pop_up_text_and_show_from_front_of_popup_queue() -> void:
 	if popup_queue.empty(): return
 	update_pop_up_text_and_show(popup_queue.pop_front())
-	print("Current size of popup_queue: " + str(popup_queue.size()))	
+	#print("Current size of popup_queue: " + str(popup_queue.size()))	
 
 func update_pop_up_text_and_show(prophecy : Prophecy) -> void:
 	popup_panel.show_up(prophecy)
